ee8732581f448e07fa6d32bb4dc4012588a80723
Kevin Britt <kbritt510@gmail.com>
2020-07-30 18:03:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,22 +27,45 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
