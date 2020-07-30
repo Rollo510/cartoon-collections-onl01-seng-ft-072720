f39b174fa29c014fb00a0ab7701679070d3ee57e
Kevin Britt <kbritt510@gmail.com>
2020-07-30 18:55:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,8 +29,8 @@ end
 
 def long_planeteer_calls(array)
   new_array = []
-  array.find do |num|
-    if any.num.length > 4
+  array.any do |num|
+    if num.length > 4
       return true
     end
   end
